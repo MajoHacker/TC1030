15584ef4c26c7e8b86e73b8dad0ae0a0acb0af02
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2023-06-06 17:40:03 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 /*
- *
  * Proyecto Agencia main
  * María José Gaytán Gil
  * A01706616
- * 26/05/2023
+ * 16/06/2023
  * 
+ * Este proyecto trata sobre una agencia de coches, 
+ * en donde cada usuario, que es un trabajador de la
+ * agencia, tiene la capacidad de agregar o quitar vehículos
+ * del catálogo de la agencia.
  */
 
-# include "Catalogo.h"
-# include <iostream>
-using namespace std;
+#include <iostream> // Para imprimir
 #include <vector>
+#include <string>
+#include "Catalogo.h" // Bibliotecas con objetos de mi proyecto
+#include "Vehiculo.h"
+
+using namespace std;
 
 int main() {
     // Variables necesarias:
@@ -27,26 +33,44 @@ int main() {
     int opcion_vehiculo;
     int indice;
 
-    // Coches creados por defecto:
-    catalogo.agregar_vehiculo(new Coche("Ford", "Focus", 2021, 200000.0, "normal"));
-    catalogo.agregar_vehiculo(new Moto("Ducati", "Panigale V4", 2022, 50000.0, 2));
-    catalogo.agregar_vehiculo(new Camioneta("Jeep", "Wrangler", 2020, 300000.0, "Deportiva"));
-    catalogo.agregar_vehiculo(new Moto("Yamaha", "YZF-R6", 2017, 57600.0, 3));
+    // Coches creados por default:
+    Vehiculo* cochedefault(new Coche("Ford", "Focus", 2021, 200000.0, "normal"));
+    Vehiculo* motodefault(new Moto("Ducati", "Panigale V4", 2022, 50000.0, 2));
+    Vehiculo* camionetadefault(new Camioneta("Jeep", "Wrangler", 2020, 300000.0, "Deportiva"));
+    catalogo.agregar_vehiculo(cochedefault);
+    catalogo.agregar_vehiculo(motodefault);
+    catalogo.agregar_vehiculo(camionetadefault);
+    
 
     // Registro de usuario:
-    cout << "Ingresa tu usuario para comenzar: ";
-    cin >> usuario;
-    cout << "\nPerfecto, " << usuario << " comencemos:" << endl;
+    do {
+        cout << "Ingresa tu nombre de usuario: ";
+        cin >> usuario;
+
+        // Condición para validar usuario
+        if (usuario == "benjamin" or usuario == "majo") {
+            cout << "Acceso concedido" << endl;
+            break;
+        } 
+        else {
+            cout << "Usuario incorrecto. Intenta nuevamente." << endl;
+        }
+    } 
+    while (true);
 
     // Ciclo do-while para preguntarle varias veces al usuario la opción que desea:
     do {
         // Opciones para el usuario:
-        cout << "\nLas opciones disponibles son:\n1-Ver catálogo\n2-Agregar vehículo\n3-Eliminar vehículo\n4-Terminar programa" << endl;
-        cout << "\n¿Qué deseas hacer?: ";
+        cout << "\nLas opciones disponibles son:\n";
+        cout << "1-Ver catálogo\n";
+        cout << "2-Agregar vehículo\n";
+        cout << "3-Eliminar vehículo\n";
+        cout << "4-Terminar programa\n";
+        cout << "¿Qué deseas hacer? ";
         cin >> respuesta;
 
         // Ciclo while para validar respuestas:
-        while (respuesta != 1 and respuesta != 2 and respuesta != 3 and respuesta != 4) {
+        while (respuesta != 1 and respuesta != 2 and respuesta != 3 and respuesta != 4){
             cout << "Respuesta inválida. Por favor, ingresa 1, 2, 3 o 4." << endl;
             cin >> respuesta;
         }
@@ -54,9 +78,9 @@ int main() {
         // Condiciones para realizar lo que el usuario quiera:
         if (respuesta == 1) {
             catalogo.mostrar_catalogo();
-        }
+        } 
         else if (respuesta == 2) {
-            cout << "Ingrese la marca del vehículo: ";
+            cout << "\nIngrese la marca del vehículo: ";
             cin >> marca;
             cout << "Ingrese el modelo del vehículo: ";
             cin >> modelo;
@@ -80,24 +104,27 @@ int main() {
             if (opcion_vehiculo == 1) {
                 cout << "Ingresa el número de llantas de la moto: ";
                 cin >> num_llantas;
-                catalogo.agregar_vehiculo(new Moto(marca, modelo, anio, precio, num_llantas));
-                cout << "Vehículo añadido correctamente." << endl;
-            }
+                Vehiculo* vehiculo(new Moto(marca, modelo, anio, precio, num_llantas));
+                catalogo.agregar_vehiculo(vehiculo);
+                cout << "\nVehículo añadido correctamente." << endl;
+            } 
             else if (opcion_vehiculo == 2) {
                 cout << "Ingresa el tipo de coche: ";
                 cin >> tipo_coche;
-                catalogo.agregar_vehiculo(new Coche(marca, modelo, anio, precio, tipo_coche));
-                 cout << "Vehículo añadido correctamente." << endl;
-            }
+                Vehiculo* vehiculo(new Coche(marca, modelo, anio, precio, tipo_coche));
+                catalogo.agregar_vehiculo(vehiculo);
+                cout << "\nVehículo añadido correctamente." << endl;
+            } 
             else if (opcion_vehiculo == 3) {
                 cout << "Ingresa el tipo de camioneta: ";
                 cin >> tipo_camioneta;
-                catalogo.agregar_vehiculo(new Camioneta(marca, modelo, anio, precio, tipo_camioneta));
-                cout << "Vehículo añadido correctamente." << endl;
+                Vehiculo* vehiculo(new Camioneta(marca, modelo, anio, precio, tipo_camioneta));
+                catalogo.agregar_vehiculo(vehiculo);
+                cout << "\nVehículo añadido correctamente." << endl;
             }
-        }
+        } 
         else if (respuesta == 3) {
-            cout << "NOTA: El indice empieza en 0" << endl;
+            catalogo.mostrar_catalogo();
             cout << "Ingrese el índice del vehículo que desea eliminar: ";
             cin >> indice;
             catalogo.eliminar_vehiculo(indice);
@@ -105,6 +132,6 @@ int main() {
         }
     } 
     while (respuesta != 4);
-        cout << "\nGracias por usar el programa. ¡Hasta luego!" << endl;
+        cout << "\nHasta luego, " << usuario << "!" << endl;
     return 0;
 };
